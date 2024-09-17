42c1d3f1fd7637e4a8885745b6a0f8c114277be3
dbahiense <douglas@rohling.com.br>
2024-09-16 22:42:17 -0300

    style: use new fonts (inter and spaceGrotesk)

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,14 +1,25 @@
 import localFont from "next/font/local";
 
 export const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 
 export const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
+export const inter = localFont({
+  src: "./InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 900",
+});
+
+export const spaceGrotesk = localFont({
+  src: "./SpaceGroteskVF.ttf",
+  variable: "--font-spaceGrotesk",
+  weight: "300 700",
+});
