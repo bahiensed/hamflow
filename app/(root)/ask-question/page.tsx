e061d6f93b3a0e49dbe10596c75bee952f6063f8
dbahiense <douglas@rohling.com.br>
2024-09-19 17:43:41 -0300

    feature: create ask question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Question from '@/components/forms/Question'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-const AskQuestion = () => {
+const AskQuestion = async () => {
+  const { userId } = "user_2mAaaA5xsu2luaZYoKSTN7Ic7kQ";
+
+  if (!userId) redirect('/sign-in');
+
+  const mongoUser = await getUserById({ userId });
+
   return (
-    <div>AskQuestion</div>
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask Question</h1>
+
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)}/>
+      </div>
+    </div>
   )
 }
 
-export default AskQuestion
+export default AskQuestion;
