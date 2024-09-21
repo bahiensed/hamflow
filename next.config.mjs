7974f2cdb5ca57e0670258436b7fefaeed201717
dbahiense <douglas@rohling.com.br>
2024-09-21 17:53:50 -0300

    chore: add domain to config file to show images from outside

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',       // leave empty if no port is specified
+        pathname: '**', // allows all paths from this hostname
+      },
+    ],
+  },
+};
 
 export default nextConfig;
