3c7b907c188f3f112aee48d94ed18dabce5bf1cc
dbahiense <douglas@rohling.com.br>
2024-09-21 23:14:23 -0300

    style: change logo and site name size and font weight and also padding

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,23 +1,22 @@
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Theme from './Theme'
-import MobileNav from './MobileNav'
-import GlobalSearch from '../search/GlobalSearch'
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+import MobileNav from '@/components/shared/navbar/MobileNav';
+import Theme from '@/components/shared/navbar/Theme';
+import GlobalSearch from '@/components/shared/search/GlobalSearch';
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center gap-2.5">
+    <nav className="background-light900_dark200 fixed z-50 flex w-full justify-between px-8 py-1.5">
+      <Link href="/" className="flex items-center gap-x-2.5">
         <Image
           src="/assets/images/site-logo.svg"
-          width={23}
-          height={23}
+          width={32}
+          height={32}
           alt="Ham Flow"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Ham <span className="text-primary-500">Flow</span></p>
+        <p className="h1-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Ham <span className="text-primary-500">Flow</span></p>
       </Link>
 
       <GlobalSearch />
