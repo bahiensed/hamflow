ccd77a49548c964e43a2844e3ea1d5f0e8e6c6c6
dbahiense <douglas@rohling.com.br>
2024-09-28 19:11:54 -0300

    refactor: update theme switcher layout and icon sizes in Theme component

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import React from 'react'
-
+import Image from 'next/image';
 import { useTheme } from '@/context/ThemeProvider';
+import { themes } from '@/constants';
 
 import {
   Menubar,
@@ -11,8 +11,6 @@ import {
   MenubarMenu,
   MenubarTrigger,
 } from "@/components/ui/menubar"
-import Image from 'next/image';
-import { themes } from '@/constants';
 
 const Theme = () => {
   const { mode, setMode } = useTheme();
@@ -20,26 +18,31 @@ const Theme = () => {
   return (
     <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
-        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+        <MenubarTrigger className="cursor-pointer focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
           {mode === 'light' ? (
-            <Image 
-            src="/assets/icons/sun.svg" alt="sun" 
-            width={20} 
-            height={20} className="active-theme" 
+            <Image
+              src="/assets/icons/sun.svg"
+              alt="sun" 
+              width={24} 
+              height={24}
+              className="active-theme" 
             />
           ): (
-          <Image 
-          src="/assets/icons/moon.svg" alt="moon"
-           width={20} 
-           height={20} className="active-theme" 
-           />
-           )}
+            <Image 
+              src="/assets/icons/moon.svg"
+              alt="moon"
+              width={18} 
+              height={18}
+              className="active-theme" 
+            />
+          )}
         </MenubarTrigger>
-        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border bg-light-900 py-2 dark:border-dark-400 dark:bg-dark-300">
+
+        <MenubarContent className="absolute -right-12 min-w-[128px] rounded border bg-light-900 py-1 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((item) => (
             <MenubarItem
               key={item.value}
-              className='flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400'
+              className='flex cursor-pointer items-center gap-1 p-2 focus:bg-slate-100 dark:focus:bg-slate-800'
               onClick={() => {
                 setMode(item.value);
                 if (item.value !== "system") {
@@ -65,4 +68,4 @@ const Theme = () => {
   )
 }
 
-export default Theme
+export default Theme;
